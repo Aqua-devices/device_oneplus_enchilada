bcc1e15808fa79d7b650ac46af6855cf8483d20e
rc420head <dennislebhardt79@gmail.com>
2020-02-17 23:57:29 -0600

    AquariOS
    Change-Id: Ibe50caf0a0f646dda5068fbd11a589e36a8271d0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/aquarios_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_enchilada-user \
-    lineage_enchilada-userdebug \
-    lineage_enchilada-eng
+    aquarios_enchilada-user \
+    aquarios_enchilada-userdebug \
+    aquarios_enchilada-eng
